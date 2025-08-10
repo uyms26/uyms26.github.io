3305db273c6c89a7f575b64cb86b5d21b8e0018c
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2025-08-10 16:05:53 +0300

    fix: set Vite base to / for user site

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/uyms26.github.io/',
+  base: '/',
   plugins: [tailwindcss(), react()],
 
 })
