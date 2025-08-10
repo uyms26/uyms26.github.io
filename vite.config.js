bf3d9355fe8606bc3401cecba2e8b65c26063873
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2025-08-10 15:24:01 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/uyms26.github.io/',
   plugins: [tailwindcss(), react()],
 })
