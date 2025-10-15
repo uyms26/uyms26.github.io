78566280b767a159733787fbe94e4cd69474b1dc
hasanaliozkan-dev <hasanaliozkan@mu.edu.tr>
2025-10-15 10:50:12 +0300

    bildir gönderimi

--- src/pages/BildiriGonderimi.jsx
+++ src/pages/BildiriGonderimi.jsx
@@ -40,6 +40,8 @@ const BildiriGonderimi = () => {
                     </div>
                   </li>
 
+                  
+
                   <li className="bg-white rounded-lg p-4 shadow-sm flex items-start space-x-3">
                     
                     <div>
